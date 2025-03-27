4e1ad92721ddb5641cc7fd870223aff592c46f70
mnilsson183 <morgan.nilsson7111@icloud.com>
2025-03-27 11:08:00 -0600

    lsp and assertion changes

--- src/libs/src/assert.c
+++ src/libs/src/assert.c
@@ -1,7 +1,9 @@
 #include "../../driver/include/vga_driver.h"
 
-void assert(int ok, const char* file, const unsigned int linenumber, const char * s) {
+void assert(int ok, const char* file, const unsigned int linenumber, const char* s) {
+
     if (!ok) {
+
         write_string("Assert failed, Execution trapped \n   ( ");
         write_string(file);
         write_string(" @ ");
@@ -10,5 +12,23 @@ void assert(int ok, const char* file, const unsigned int linenumber, const char 
         write_string(s);
         write_string("\"");
         while (1);
+
     }
+
+}
+
+void warn(int ok, const char* file, const unsigned int linenumber, const char* s) {
+
+    if (!ok) {
+
+        write_string("Warn: \n");
+        write_string(file);
+        write_string(" @ ");
+        write_int(linenumber);
+        write_string(" ) \"");
+        write_string(s);
+        write_string("\"");
+
+    }
+
 }
