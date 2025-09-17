72b88f58824f32407de8f418b098631c39bd855a
morgan-nilsson <morgan.nilsson7111@icloud.com>
2025-09-17 08:51:03 -0600

    Revert "Flat only memory model for now"
    This reverts commit 167a910f4ee5968f37026c199b7ade1e81aa45cb.
    
    Bad commit

--- include/kernel/paging.h
+++
@@ -1,7 +0,0 @@
-#ifndef PAGING_H
-#define PAGING_H
-#include "libs/stdtypes.h"
-
-void init_paging();
-
-#endif
--- src/kernel/system.c
+++ src/kernel/system.c
@@ -18,6 +18,49 @@ syscall_t syscall_table[NUM_SYSCALLS] = {
     (syscall_t) sys_sleep
 };
 
+#define PAGE_PRESENT 0x1
+#define PAGE_WRITE 0x2
+#define PAGE_USER 0x4
+
+// paging code first 3gb user last 1gb kernel
+#define PDE_ENTRIES 1024
+#define PTE_ENTRIES 1024
+
+// page table entry
+static uint32_t page_directory[PDE_ENTRIES] __attribute__((aligned(4096)));
+
+// page table
+static uint32_t page_table[PDE_ENTRIES][PTE_ENTRIES] __attribute__((aligned(4096)));
+
+void init_paging() {
+
+    for (unsigned int i = 0; i < 1024; i++) {
+
+        // select flag type
+        uint32_t pde_flags = PAGE_PRESENT | PAGE_WRITE;
+        if (i < 768) pde_flags |= PAGE_USER;
+
+        for (unsigned int j = 0; j < 1024; j++) {
+            uint32_t address = (i * 1024 + j) * 0x1000;
+
+            // remove lower 12 bits and add flags
+            page_table[i][j] = (address & ~0xFFF) | pde_flags;
+        }
+
+        page_directory[i] = (uint32_t)page_table[i] | pde_flags;
+    }
+
+    // unmap null page
+    page_table[0][0] = 0;
+
+    asm volatile("mov %0, %%cr3" : : "r" (page_directory));
+
+    uint32_t cr0;
+    asm volatile("mov %%cr0, %0" : "=r" (cr0));
+    cr0 |= 0x80000000;
+    asm volatile("mov %0, %%cr0" : : "r" (cr0));
+}
+
 int sys_read(int fd, char *buf, int count, char unused, char unused2) {
     if (buf == NULL || count <= 0) {
         return -1;
